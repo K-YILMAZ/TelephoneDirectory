807965fb62516a3ca931229fadc3b5f343748dfe
K-YILMAZ <xxkanber.12345@gmail.com>
2022-05-29 16:20:32 +0300

    api gateway düzenleme

--- src/ApiGateway/WebApiGateway/TelephoneDirectoryApiGateways/Startup.cs
+++ src/ApiGateway/WebApiGateway/TelephoneDirectoryApiGateways/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,7 +28,7 @@ namespace TelephoneDirectoryApiGateways
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddOcelot();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -44,16 +46,7 @@ namespace TelephoneDirectoryApiGateways
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TelephoneDirectoryApiGateways v1"));
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+             app.UseOcelot();
         }
     }
 }
